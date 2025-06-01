ce32946616bb686bfe537fd1547f1ba8e86a3377
appkaz19 <skyt30699@gmail.com>
2025-06-01 18:23:52 +0500

    Fix modules and add README

--- tts/xtts_infer.py
+++ tts/xtts_infer.py
@@ -1,8 +1,10 @@
 from TTS.api import TTS
+import os
 
 tts = TTS(model_name="tts_models/multilingual/multi-dataset/xtts_v2", progress_bar=False)
 
 def generate_tts(text, speaker_wav_path, language, output_path="assets/tts.wav"):
+    os.makedirs(os.path.dirname(output_path), exist_ok=True)
     tts.tts_to_file(text=text,
                     speaker_wav=speaker_wav_path,
                     language=language,
