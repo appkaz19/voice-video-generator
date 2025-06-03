a396c6406c9d73a44aa17dc274500d61360b7257
appkaz19 <skyt30699@gmail.com>
2025-06-03 20:00:09 +0500

    Исправлена функция объединения аудио и видео

--- utils/video_utils.py
+++ utils/video_utils.py
@@ -1,4 +1,15 @@
 import ffmpeg
 
 def merge_audio_video(audio_path, video_path, output_path):
-    ffmpeg.input(video_path).output(audio_path, output_path, vcodec='copy', acodec='aac', strict='experimental').run()
+    """Merge separate audio and video files into a single output."""
+
+    video_input = ffmpeg.input(video_path)
+    audio_input = ffmpeg.input(audio_path)
+
+    (
+        ffmpeg
+        .output(video_input, audio_input, output_path,
+                vcodec='copy', acodec='aac', strict='experimental')
+        .overwrite_output()
+        .run()
+    )
